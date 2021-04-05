9da8c5ebcb8132dfe18b84cd8e7171742cdb18da
RobertaKelly <you@example.com>
2021-04-05 17:43:12 -0300

    fix2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const express = require('express')
 const routes = require('./server/routes/index')
 const app = express()
-const port = process.env.PORT || 5432;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
